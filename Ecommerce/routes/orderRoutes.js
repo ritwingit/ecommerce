4222d59b130fdd32cf120ae5041fe778ac003619
githuuubo <arjuntk41155@gmail.com>
2025-11-20 16:13:03 +0530

    new one

--- Ecommerce/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder, listOrders } from '../controllers/orderController.js';
-import { protect } from '../middleware/auth.js';
-
-
-const router = express.Router();
-router.post('/', protect, createOrder);
-router.get('/', protect, listOrders);
-export default router;
