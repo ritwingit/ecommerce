f2bf005fa8b685d2bf320230da90c1769f93d2f8
githuuubo <arjuntk41155@gmail.com>
2025-11-25 17:59:13 +0530

    product page published
    added product details page

--- Ecommerce/routes/productRoutes.js
+++ Ecommerce/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { addProductReview } from "../controllers/productController.js";
 import { listProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/auth.js';
 
@@ -9,4 +10,6 @@ router.get('/:id', getProduct);
 router.post('/', protect, admin, createProduct);
 router.put('/:id', protect, admin, updateProduct);
 router.delete('/:id', protect, admin, deleteProduct);
+router.post("/:id/reviews", protect, addProductReview);
+
 export default router;
