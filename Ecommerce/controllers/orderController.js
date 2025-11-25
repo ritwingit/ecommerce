d6f3e7f8afb7100fa267a594044ef8d98c92c0da
adhithck <adhithck21@gmail.com>
2025-11-25 17:58:56 +0530

    order branch

--- Ecommerce/controllers/orderController.js
+++ Ecommerce/controllers/orderController.js
@@ -1,4 +1,4 @@
-import Order from '../models/order.js';
+import Order from '../models/Order.js';
 
 
 export const createOrder = async (req, res) => {
--- Ecommerce/server.js
+++ Ecommerce/server.js
@@ -26,6 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
